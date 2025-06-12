ce8344ee638f0ca4dae901a7bf7a8386cfb08cdf
Денис <109965453+Allzer@users.noreply.github.com>
2025-06-12 15:49:16 +0300

    добавил доп стиль для телефона

--- run.py
+++ run.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+app.secret_key = 'jhasdfaslkj;d;skaojshdasdasdsfhgjhg;ohi'
 
 @app.route('/')
-def home():
-    return render_template('index.html')
+def index():
+    user_agent = request.user_agent.string
+    if 'Mobile' in user_agent or 'Android' in user_agent or 'iPhone' in user_agent:
+        return render_template('index_phone.html')
+    else:
+        return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
